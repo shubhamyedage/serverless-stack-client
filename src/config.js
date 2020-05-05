97196a3a08610b894cb1070454c162a7cce49d2c
shubhamyedage <shubham.yedage@synerzip.com>
2020-05-05 17:08:22 +0530

    Seeting react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-south-1",
+      BUCKET: "custom-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "ap-south-1",
+      URL: "https://34y3gnayrj.execute-api.ap-south-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_Sgbzbkg75",
+      APP_CLIENT_ID: "67e4mtsut1gn38o5tt1hemhrij",
+      IDENTITY_POOL_ID: "ap-south-1:9e724171-610d-4382-951a-0f95a05b5e79"
+    }
+  };
